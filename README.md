77b0bd29df4e0231adb5b6a366c222c156f8105d
Ludovic Trottier <ludovic.trottier.2@gmail.com>
2018-03-22 18:01:23 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # ZCA-Whitening-Python
 ZCA Whitening in Python
+
+
+
+#### Example
+
+```python
+from zca import ZCA
+import numpy as np
+
+N = 100
+K= 4
+
+data = np.random.rand(N, K)
+
+zca = ZCA()
+
+# Separate fit and transform
+zca.fit(data)
+data_zca = zca.transform(data)
+
+# Or joint
+data_zca = zca.fit_transform(data)
+```
--- zca.py
+++ zca.py
@@ -1,3 +1,4 @@
+"""
 MIT License
 
 Copyright (c) 2018 Ludovic Trottier
@@ -19,3 +20,28 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
+"""
+import numpy as np
+from sklearn.utils import as_float_array
+from sklearn.base import TransformerMixin, BaseEstimator
+
+class ZCA(BaseEstimator, TransformerMixin):
+
+    def __init__(self, regularization=1e-5, copy=False):
+        self.regularization = regularization
+        self.copy = copy
+
+    def fit(self, X, y=None):
+        X = as_float_array(X, copy=self.copy)
+        self.mean_ = np.mean(X, axis=0)
+        X = X - self.mean_
+        sigma = np.dot(X.T, X) / X.shape[1]
+        U, S, V = np.linalg.svd(sigma)
+        tmp = np.dot(U, np.diag(1 / np.sqrt(S + self.regularization)))
+        self.components_ = np.dot(tmp, U.T)
+        return self
+
+    def transform(self, X):
+        X_transformed = X - self.mean_
+        X_transformed = np.dot(X_transformed, self.components_.T)
+        return X_transformed
